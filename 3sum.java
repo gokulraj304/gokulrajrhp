5d2b291357adf8000cffa3b421fd01e95d081b9d
GOKULRAJ  A R <240606le.cs@rmkec.ac.in>
2026-07-29 08:56:07 +0530

    3sum.java

--- 3sum.java
+++ 3sum.java
@@ -1,38 +1,29 @@
-import java.util.*;
-
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-
-        List<List<Integer>> result = new ArrayList<>();
-
+        List<List<Integer>> res=new ArrayList<>();
         Arrays.sort(nums);
-
-        for (int i = 0; i < nums.length - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
-            }
-            int left = i + 1;
-            int right = nums.length - 1;
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-                if (sum == 0) {
-                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
-                    left++;
-                    right--;
-                    while (left < right && nums[left] == nums[left - 1]) {
-                        left++;
+        int n=nums.length;
+        for(int i=0;i<n-2;i++){
+            if(nums[i]>0) break;
+            if(i!=0 && nums[i]==nums[i-1]) continue;
+            int l=i+1,r=n-1;
+            while(l<r){
+                int sum=nums[i]+nums[l]+nums[r];
+                if(sum==0){
+                    res.add(Arrays.asList(nums[i],nums[l],nums[r]));
+                    while(l+1<n && nums[l]==nums[l+1]){
+                        l++;
                     }
-                    while (left < right && nums[right] == nums[right + 1]) {
-                        right--;
-                    }
-                } else if (sum < 0) {
-                    left++;
-                } else {
-                    right--;
+                    l++;
+                }
+                else if(sum<0){
+                    l++;
+                }
+                else{
+                    r--;
                 }
             }
         }
-
-        return result;
+        return res;
     }
-}
+    }
